df8814237084f347e94e5bc12aa385d02237a541
ArshWalker <arshwalker7@gmail.com>
2021-08-25 16:25:55 +0530

    ChartBar.js added

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,5 +1,24 @@
 // import React from 'react';
 import "./ChartBar.css";
-const ChartBar = () => {};
+const ChartBar = (props) => {
+  let barFillHeight = "0%";
+
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+
+  // this logic is used for the height if max is 100 and we have value 50 it will fill the 50% height of barFillHeight
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
 
 export default ChartBar;

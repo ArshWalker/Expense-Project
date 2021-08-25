cc13a3103b4843fe9f0c34fa0d0eb28ba102c1bd
ArshWalker <arshwalker7@gmail.com>
2021-08-25 16:24:06 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
+import Footer from "./components/Footer/Footer";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const DUMMY_EXPENSES = [
   {
     id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
+    title: "Samsung Galaxy M31",
+    amount: 16500.12,
     date: new Date(2020, 7, 14),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e2",
+    title: "Tennis Rackets",
+    amount: 696.96,
+    date: new Date(2021, 2, 12),
+  },
   {
     id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
+    title: "Books",
+    amount: 794.67,
     date: new Date(2021, 2, 28),
   },
   {
     id: "e4",
     title: "New Desk (Wooden)",
-    amount: 450,
+    amount: 1450,
     date: new Date(2021, 5, 12),
   },
 ];
@@ -37,6 +43,7 @@ function App() {
     <div>
       <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       <Expenses items={expenses}></Expenses>
+      <Footer />
     </div>
   );
 }
